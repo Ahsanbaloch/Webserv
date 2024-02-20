ed28df0672c46c7b8386be64926b77dc49557015
Ahsan Abdul Salam <ahsalam@c2r12s3.42wolfsburg.de>
2024-02-20 16:20:04 +0100

    switched from marius directory to other

--- srcs/RequestHandler.cpp
+++ srcs/RequestHandler.cpp
@@ -17,8 +17,13 @@ void	RequestHandler::handleRequest(int event_lst_item_fd)
 	int bytes_read = recv(event_lst_item_fd, buf, sizeof(buf), 0);
 	if (bytes_read == -1)
 		throw CustomException("Failed when processing read request\n");
-		
-	printf("read %i bytes\n", bytes_read);	
+	else if (bytes_read == 0)
+	{
+		std::cout << "client disconnected\n";
+		close(event_lst_item_fd);
+	}
+	else
+		printf("read %i bytes\n", bytes_read);	
 
 	// close fd in case bytes_read == 0 ???
 }
