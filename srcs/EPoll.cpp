e93835725d64d6d4df743db346b90313e9666cfb
mamesser <mamesser@student.42wolfsburg.de>
2024-02-20 14:24:54 +0100

    fix bugs in epoll class

--- srcs/EPoll.cpp
+++ srcs/EPoll.cpp
@@ -24,20 +24,21 @@ void	EPoll::attachListeningSockets()
 		struct epoll_event listening_event;
 		listening_event.data.fd = SocketsBlock.listening_sockets[i].getSocketFd();
 		listening_event.events = EPOLLIN;
-		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, SocketsBlock.listening_sockets[i], &listening_event) == -1)
+		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, SocketsBlock.listening_sockets[i].getSocketFd(), &listening_event) == -1)
 			throw CustomException("Failed when registering events for listening sockets\n");
 	}
 }
 
 void	EPoll::attachConnectionSockets(std::vector<int> pending_fds)
 {
+	int size = pending_fds.size();
 	struct epoll_event connection_event;
-	for (int i = 0; i < pending_fds.size(); i++)
+	for (int i = 0; i < size; i++)
 	{
 		ListeningSocket::setNonblocking(pending_fds[i]); // check if this can be done without fcntl for epoll
 		connection_event.data.fd = pending_fds[i];
 		connection_event.events = EPOLLIN; // might also use EPOLLOUT
-		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pending_fds[i], &conncetion_event) == -1)
+		if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, pending_fds[i], &connection_event) == -1)
 			throw CustomException("Failed when registering events for conncetion sockets\n");
 	}
 }
