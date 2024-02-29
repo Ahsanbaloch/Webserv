e32671ca2b71955bbd01c39dbe0d5d8cb14f5f6c
mamesser <mamesser@student.42wolfsburg.de>
2024-02-29 15:53:43 +0100

    parse headers in http request

--- includes/RequestHandler.h
+++ includes/RequestHandler.h
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include "CustomException.h"
 #include <string>
+#include <map>
 #include <cstdio> // remove later
 
 #define BUFFER_SIZE 1024
@@ -20,18 +21,21 @@ public:
 	RequestHandler(/* args */);
 	~RequestHandler();
 
-	std::string	method; // probably needs to be reset after being used
-	std::string path; // probably needs to be reset after being used
-	std::string query; // probably needs to be reset after being used
-	std::string version; // probably needs to be reset after being used
-	int			error; // probably needs to be reset after being used
-	char		buf[BUFFER_SIZE]; // use std::vector<char> buf(BUFFER_SIZE); instead?
-	int			buf_pos;
-	int			bytes_read;
-	int			parsing_done; // probably needs to be reset after being used
+	std::string							method; // probably needs to be reset after being used
+	std::string							path; // probably needs to be reset after being used
+	std::string							query; // probably needs to be reset after being used
+	std::string							version; // probably needs to be reset after being used
+	std::map<std::string, std::string>	headers; // probably needs to be reset after being used
+	int									error; // probably needs to be reset after being used
+	char								buf[BUFFER_SIZE]; // use std::vector<char> buf(BUFFER_SIZE); instead?
+	int									buf_pos;
+	int									bytes_read;
+	int									rl_parsing_done; // probably needs to be reset after being used
+	int									headers_parsing_done;
 
 	void	handleRequest(int);
 	void	parseRequestLine();
+	void	parseHeaders();
 	void	checkMethod();
 	void	checkHttpVersion();
 
@@ -47,7 +51,18 @@ public:
 		rl_http,
 		rl_cr,
 		rl_done
-	} state;
+	} rl_state;
+
+	enum {
+		he_start = 0,
+		he_name,
+		he_ws_before_value,
+		he_value,
+		he_ws_after_value,
+		he_cr,
+		he_done,
+		hes_end
+	} headers_state;
 };
 
 #endif
--- srcs/RequestHandler.cpp
+++ srcs/RequestHandler.cpp
@@ -5,7 +5,8 @@ RequestHandler::RequestHandler(/* args */)
 {
 	error = 0;
 	buf_pos = -1;
-	parsing_done = 0;
+	rl_parsing_done = 0;
+	headers_parsing_done = 0;
 	method = ""; // does this reset the string?
 	path = ""; // does this reset the string?
 	query = ""; // does this reset the string?
@@ -19,16 +20,24 @@ RequestHandler::~RequestHandler()
 
 // read request handler
 void	RequestHandler::handleRequest(int event_lst_item_fd)
-{
-	// char buf[BUFFER_SIZE]; // use std::vector<char> buf(BUFFER_SIZE); instead?
-	
+{	
 	bytes_read = recv(event_lst_item_fd, buf, sizeof(buf), 0);
 	if (bytes_read == -1)
 		throw CustomException("Failed when processing read request\n");
 	buf[bytes_read] = '\0'; // correct or bytes_read +1?
-	
-	printf("buf: %s\n\n", buf);
+
+	printf("buf: %s\n", buf);
+
 	parseRequestLine();
+	buf_pos--; // put into request line parser?
+	parseHeaders();
+	
+	printf("\nheaders\n");
+	for (std::map<std::string, std::string>::iterator it = headers.begin(); it != headers.end(); it++)
+	{
+		std::cout << "key: " << it->first << " ";
+		std::cout << "value: " << it->second << std::endl;
+	}
 
 	std::cout << "identified method: " << method << '\n';
 	std::cout << "identified path: " << path << '\n';
@@ -39,6 +48,153 @@ void	RequestHandler::handleRequest(int event_lst_item_fd)
 	printf("read %i bytes\n", bytes_read);	
 
 	// close fd in case bytes_read == 0 ???
+}
+
+void	RequestHandler::parseHeaders()
+{
+	u_char		ch;
+	std::string	header_name = "";
+	std::string	header_value = "";
+
+	headers_state = he_start;
+
+	while (!headers_parsing_done && buf_pos++ < bytes_read)
+	{
+		ch = buf[buf_pos];
+	
+		switch (headers_state) 
+		{
+			case he_start:
+				if (ch == CR)
+				{
+					headers_state = hes_end;
+					break;
+				}
+				else if (ch == LF)
+				{
+					headers_parsing_done = 1;
+					break;
+				}
+				else
+					headers_state = he_name;
+			
+			case he_name: // currently not allowing whitespace preceding the field name // allowed chars: //  A-Z, a-z, 0–9, hyphen ( - ), or underscore ( _ ).
+				if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || ch == '-' || ch == '_')
+				{
+					if (ch >= 'A' && ch <= 'Z')
+						header_name.append(1, std::tolower(ch));
+					else
+						header_name.append(1, ch);
+				}
+				else if (ch == ':')
+					headers_state = he_ws_before_value;
+				else if (ch == CR)
+					headers_state = he_cr;
+				else if (ch == LF)
+					headers_state = he_done;
+				else
+				{
+					error = 400;
+					throw CustomException("Bad request");
+				}
+				break;
+
+			case he_ws_before_value:
+				if (ch == SP)
+					break;
+				else if (ch == CR)
+				{
+					headers_state = he_cr;
+					break;
+				}
+				else if (ch == LF)
+				{
+					headers_state = he_done;
+					break;
+				}
+				else
+					headers_state = he_value;
+
+
+			case he_value:
+				if (ch == SP)
+					headers_state = he_ws_after_value;
+				else if (ch == CR)
+					headers_state = he_cr;
+				else if (ch == LF)
+					headers_state = he_done;
+				else if (ch > 32 && ch < 127) // accepted: a-z, A-Z, and 0-9 _ :;.,\/"'?!(){}[]@<>=-+*#$&`|~^%
+					header_value.append(1, ch);
+				else
+				{
+					error = 400;
+					throw CustomException("Bad request");
+				}
+				break;
+			
+			case he_ws_after_value:
+				if (ch == SP)
+					break;
+				else if (ch == CR)
+				{
+					headers_state = he_cr;
+					break;
+				}
+				else if (ch == LF)
+				{
+					headers_state = he_done;
+					break;
+				}
+				else
+				{
+					headers_state = he_value;
+					header_value.append(1, SP);
+					header_value.append(1, ch);
+					break;
+				}
+
+			case he_cr:
+				if (ch == LF)
+					rl_state = rl_done;
+				else 
+				{
+					error = 400;
+					throw CustomException("Bad request");
+				}
+			
+			case he_done:
+				std::cout << "header line fully parsed\n";
+				headers.insert(std::pair<std::string, std::string>(header_name, header_value));
+				header_name.clear();
+				header_value.clear();
+				headers_state = he_start;
+				break;
+
+			case hes_end:
+				if (ch == LF)
+				{
+					headers_parsing_done = 1;
+					std::cout << "headers fully parsed\n";
+					break;
+				}
+				error = 400;
+				throw CustomException("Bad request");
+		}
+	}
+
+
+	// A sender MUST NOT send whitespace between the start-line and the first header field.
+	// A recipient that receives whitespace between the start-line and the first header field MUST either reject the message as invalid 
+	// or consume each whitespace-preceded line without further processing of it (i.e., ignore the entire line, along with any subsequent lines preceded by whitespace, until a properly formed header field is received or the header section is terminated).
+	
+	// Each field line consists of a case-insensitive field name followed by a colon (":"), optional leading whitespace, the field line value, and optional trailing whitespace.
+	// No whitespace is allowed between the field name and colon --> 400 bad request
+
+	// A field line value might be preceded and/or followed by optional whitespace (OWS);
+	// The field line value does not include that leading or trailing whitespace
+	// line folding in the the "message/http" media type???
+
+	// end is signified by CRLFCRLF
 }
 
 void	RequestHandler::checkMethod()
@@ -91,6 +247,9 @@ void	RequestHandler::checkHttpVersion()
 		error = 400;
 }
 
+
+
+
 void	RequestHandler::parseRequestLine()
 {
 	u_char	ch;
@@ -106,13 +265,13 @@ void	RequestHandler::parseRequestLine()
 	// unreseved:  - _ . ~
 	// alphanumeric
 
-	state = rl_start;
+	rl_state = rl_start;
 
-	while (buf_pos++ < bytes_read && !parsing_done)
+	while (!rl_parsing_done && buf_pos++ < bytes_read)
 	{
 		ch = buf[buf_pos];
 	
-		switch (state) 
+		switch (rl_state) 
 		{
 			// in the interest of robustness, ignore at least one empty line
 			// double check if a single LF occurance is accepted // is there a space afterwards?
@@ -121,13 +280,13 @@ void	RequestHandler::parseRequestLine()
 					break;
 				if (ch == LF)
 					break;
-				state = rl_method;
+				rl_state = rl_method;
 
 			case rl_method: // can this also be termined by CRLF?
 				checkMethod();
 				if (error == 400)
 					throw CustomException("Bad request");
-				state = rl_first_divider;
+				rl_state = rl_first_divider;
 				break;
 
 			case rl_first_divider: // may add to method check
@@ -135,7 +294,7 @@ void	RequestHandler::parseRequestLine()
 					break;
 				else if (ch == '/')
 				{
-					state = rl_path;
+					rl_state = rl_path;
 					path.append(1, static_cast<char>(ch));
 					break;
 				}
@@ -149,24 +308,24 @@ void	RequestHandler::parseRequestLine()
 				switch (ch)
 				{
 					case SP:
-						state = rl_http;
+						rl_state = rl_http;
 						break;
 					case CR: // what if this occurs and no http version is specified?
-						state = rl_cr;
+						rl_state = rl_cr;
 						break;
 					case LF: // what if this occurs and no http version is specified?
-						state = rl_done;
+						rl_state = rl_done;
 						break;
 					case '.': // nginx does not allow two dots at the beginning if nothing comes after; a single dot leads to index (also on some websites with two dots); three dots to file not found
 						break;
 					case '%':
 						path_encoded = 1; // when interpreting request needs to be decoded
-						state = rl_percent_encoded; // need to be followed by numercial code that needs to be interpreted --> otherwise 400 bad request (not the case for query)
+						rl_state = rl_percent_encoded; // need to be followed by numercial code that needs to be interpreted --> otherwise 400 bad request (not the case for query)
 						break;
 					case '/': // checking with nginx, there can be several slashes after each other
 						break;
 					case '?':
-						state = rl_query;
+						rl_state = rl_query;
 						break;
 					case '+': // means space; needs to be decoded when interpreting request
 						path_encoded = 1;
@@ -182,7 +341,7 @@ void	RequestHandler::parseRequestLine()
 					// case '#': // in what case is this character even encountered in a http message? probably only in the query part and when interpreting // fragments are not sent in the http request messages. The reason is that the fragment identifier is only used by the browser
 
 				}
-				if (state == rl_path || state == rl_percent_encoded)
+				if (rl_state == rl_path || rl_state == rl_percent_encoded)
 					path.append(1, static_cast<char>(ch));
 				break;
 
@@ -192,7 +351,7 @@ void	RequestHandler::parseRequestLine()
 					loops++;
 					path.append(1, static_cast<char>(ch));
 					if (loops % 2 == 0)
-						state = rl_path;
+						rl_state = rl_path;
 					break;
 				}
 				error = 400;
@@ -202,13 +361,13 @@ void	RequestHandler::parseRequestLine()
 				switch (ch)
 				{
 					case SP:
-						state = rl_http;
+						rl_state = rl_http;
 						break;
 					case CR:
-						state = rl_cr;
+						rl_state = rl_cr;
 						break;
 					case LF:
-						state = rl_done;
+						rl_state = rl_done;
 						break;
 					default:
 						if (ch < 32 || ch == 127)
@@ -218,19 +377,19 @@ void	RequestHandler::parseRequestLine()
 						}
 						break;
 				}
-				if (state == rl_query)
+				if (rl_state == rl_query)
 					query.append(1, static_cast<char>(ch));
 				break;
 
 			case rl_http: // do we need to check for diffferent versions?
 				if (ch == CR)
 				{
-					state = rl_cr;
+					rl_state = rl_cr;
 					break;
 				}
 				if (ch == LF)
 				{
-					state = rl_done;
+					rl_state = rl_done;
 					break;
 				}
 				if (ch == 'H')
@@ -241,14 +400,16 @@ void	RequestHandler::parseRequestLine()
 
 			case rl_cr:
 				if (ch == LF)
-					state = rl_done;
-				break;
-				error = 400;
-				throw CustomException("Bad request");
+					rl_state = rl_done;
+				else 
+				{
+					error = 400;
+					throw CustomException("Bad request");
+				}
 			
 			case rl_done:
 				std::cout << "request line fully parsed\n";
-				parsing_done = 1;
+				rl_parsing_done = 1;
 				break;
 		}
 
