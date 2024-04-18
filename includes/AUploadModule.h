49c1eb66fc1cdcda7f2ea86e4d30b1fb3fa91716
mamesser <mamesser@student.42wolfsburg.de>
2024-04-18 18:16:28 +0200

    clean AUploadModule class

--- includes/AUploadModule.h
+++ includes/AUploadModule.h
@@ -14,29 +14,11 @@ protected:
 	RequestHandler&	handler;
 
 	// vars
-	// int				chunk_length;
-	// int				total_chunk_size;
 	std::string			filename; // move to children? (if not needed in all sub-classes)
-	// std::ofstream	temp_chunked;
 
 	// flags
 	bool			body_read;
 	bool			body_parsing_done; // only needed in Multipart class
-	// bool			trailer_exists;
-
-	// // states
-	// enum {
-	// 	body_start = 0,
-	// 	chunk_size,
-	// 	chunk_size_cr,
-	// 	chunk_extension,
-	// 	chunk_data,
-	// 	chunk_data_cr,
-	// 	chunk_trailer,
-	// 	chunk_trailer_cr,
-	// 	body_end_cr,
-	// 	body_end
-	// } te_state;
 
 	// constructors
 	AUploadModule();
@@ -53,8 +35,6 @@ public:
 
 	// methods
 	virtual void	readBody() = 0;
-	// void			unchunkBody();
-	// void			storeChunkedData();
 
 };
 
